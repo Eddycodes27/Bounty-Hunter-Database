a83cf2ff7a10d7cadf10f589b01a2d66f192eaf9
Eddycodes27 <Eddycodes27@gmail.com>
2021-12-10 10:24:32 -0500

    Added project to repo

--- src/App.js
+++ src/App.js
@@ -1,44 +1,52 @@
+import logo from './logo.svg';
 import './App.css';
-import { useEffect, useState } from 'react'
+import { useEffect, useState } from 'react';
 import Poster from './Poster'
 import Display from './Display'
 import Form from './Form'
 
 function App() {
-
   const [bounties, setBounties] = useState([])
   const [current, setCurrent] = useState({})
 
-  useEffect(() => {
-    fetch('http://localhost:8000/bounties')
-      .then(response => response.json())
-      .then(foundBounties => {
-        setBounties(foundBounties)
-      })
-  },[])
+  useEffect(()=>{
+    getBounties()
+  }, [])
 
-  const changeCurrent = bounty => {
+  const getBounties = () => {
+    fetch('http://localhost:8000/bounties')
+    .then(response=>{
+      return response.json()
+    })
+    .then(foundBounties=>{
+      console.log(foundBounties)
+      setBounties(foundBounties)
+    })
+  }
+
+  const changeCurrent = (bounty) => {
     setCurrent(bounty)
   }
 
-const posters = bounties.map(b => {
-  return <Poster bounty={b} key={b._id} changeCurrent={changeCurrent}/>
-})
+  let posters = bounties.map(b=>{
+    return <Poster bounty={b} key={b.name} changeCurrent={changeCurrent}/>
+  })
 
   return (
     <div className="App">
       <header className="App-header">
         <h1>Wanted</h1>
-        <Display bounty={current}/>
+        <Display bounty={current} />
+        
       </header>
-      <section className='Poster-Board'>
-        <p>{posters}</p>
+      <section>
+        {posters}
       </section>
-      <section className='App-header'>
-      <Form />
+      <section className="App-header">
+        <Form refreshBounties={getBounties}/>
       </section>
     </div>
-  );
+  )
 }
 
 export default App;
